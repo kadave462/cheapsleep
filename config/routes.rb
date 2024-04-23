2c64240ade2f87bcf2140dc42db1bf344c338107
kabera david <davidmuhirekabera462@gmail.com>
2024-04-23 15:59:30 +0200

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   get 'bookings/create'
   get 'rooms/index'
   get 'rooms/show'
-  devise_for :users
   root to: "pages#home"
 
-    resources :rooms, only: [:index, :show] do
-      resources :bookings, only: [:new, :create]
-    end
+  resources :rooms, only: [:index, :show] do
+    resources :bookings, only: [:new, :create]
+  end
+  devise_for :users
      resources :bookings, only: [:index]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
